b33696573795e12c450b1ae54cfe0daebd83dd41
bharath yallala <yallalabharath00@gmail.com>
2026-02-09 20:34:53 +0530

    services updated

---
+++ src/pages/Logs.jsx
@@ -0,0 +1,13 @@
+const Logs = () => {
+  return (
+    <div>
+      <h2>System Logs</h2>
+      <ul>
+        <li>Login success - admin</li>
+        <li>Firewall rule updated</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Logs;
---
+++ src/pages/settings.jsx
@@ -0,0 +1,10 @@
+const Settings = () => {
+  return (
+    <div>
+      <h2>User Roles & Config</h2>
+      <p>Admin / Analyst / Viewer</p>
+    </div>
+  );
+};
+
+export default Settings;
