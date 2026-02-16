1238dc1d5b00aa970a8f2062bfb55bd91a79bcd6
bharath yallala <yallalabharath00@gmail.com>
2026-02-16 14:51:14 +0530

    App.jsx conflict resolved

---
+++ src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import { login } from "../services/authService";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const Login = () => {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    await login(user, pass);
+    navigate("/dashboard");
+  };
+
+  return (
+    <div>
+      <h2>Secure Login</h2>
+      <input placeholder="Username" onChange={e => setUser(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={e => setPass(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
