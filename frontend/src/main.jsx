e08de9e20a9cb3d74ddac775e918e92903bb7e64
bharath yallala <yallalabharath00@gmail.com>
2026-02-18 18:04:07 +0530

    login.jsx and main,jsx is updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Provider } from "@/components/ui/provider"
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
