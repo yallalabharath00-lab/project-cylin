26518c34426492562a859b70d8af11b5e03db1e6
bharath yallala <yallalabharath00@gmail.com>
2026-02-19 19:26:41 +0530

    Removed duplicate root Vite project

--- src/services/alertService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getAlerts = async () => {
-  const res = await api.get("/alerts");
-  return res.data;
-};
--- src/services/logService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getLogs = async () => {
-  const res = await api.get("/logs");
-  return res.data;
-};
